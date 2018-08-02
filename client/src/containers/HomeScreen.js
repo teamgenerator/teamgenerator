df36327d878521c6847cca4dfe58c38233770d5b
Michael Englo <menglo@optimalefficiency.net>
2018-08-02 00:00:28 -0700

    make rough design for home screen

--- client/src/containers/HomeScreen.js
+++ client/src/containers/HomeScreen.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { Button, Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  actions: {
+    marginTop: 30,
+    flex: 1,
+  },
+};
 
 class HomeScreen extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classes.container}>
+        <Grid container>
+          <Grid item xs>
+            <Button variant="extendedFab" color="secondary" fullWidth className={classes.actions}>
+              Generate Teams
+            </Button>
+
+            <Button variant="extendedFab"  color="secondary" fullWidth className={classes.actions}>
+              Add Players
+            </Button>
+
+            <Button variant="extendedFab"  color="secondary" fullWidth className={classes.actions}>
+              Edit Players
+            </Button>
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default HomeScreen;
+export default withStyles(styles)(HomeScreen);
