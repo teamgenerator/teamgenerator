3a34d3458be775f8399c7cdcba5afe7620438dc2
Michael Englo <menglo@optimalefficiency.net>
2018-08-01 22:33:45 -0700

    init react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './containers/Home';
 
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        
+        <BrowserRouter>
+          <Route exact path="/" component={Home} />
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
