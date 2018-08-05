4a9a6e4f9091de1e7a749094dd1d65513954a195
nicoalimin <nicoalimin@yahoo.com>
2018-08-05 01:34:15 -0700

    added Community Model

---
+++ server/Models/community.go
@@ -0,0 +1,18 @@
+////
+// author: Nico Alimin (nico@hackcapital.com)
+// date: Sunday, 5th August 2018 1:24:59 am
+// lastModifiedBy: Nico Alimin (nico@hackcapital.com)
+// lastModifiedTime: Sunday, 5th August 2018 1:30:28 am
+//
+// DESCRIPTION
+//
+// copyright (c) 2018 Nico Alimin
+////
+
+package main
+
+type community struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Location string `json:"location`
+}
