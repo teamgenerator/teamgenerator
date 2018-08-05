ecae8ca70f1d9dc356708cec5363fd543452069a
nicoalimin <nicoalimin@yahoo.com>
2018-08-05 03:42:13 -0700

    disabled ssl

--- server/app.go
+++ server/app.go
@@ -30,7 +30,7 @@ type App struct {
 
 func (a *App) Initialize(user, password, dbname string) {
 	connectionString :=
-		fmt.Sprintf("user=%s password=%s dbname=%s", user, password, dbname)
+		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 
 	var err error
 	a.DB, err = sql.Open("postgres", connectionString)
