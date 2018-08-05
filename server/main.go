529e6b0f53dd0403ca2650f3be618405e44be7ab
nicoalimin <nicoalimin@yahoo.com>
2018-08-05 00:54:40 -0700

    added init files

---
+++ server/main.go
@@ -0,0 +1,16 @@
+////
+// author: Nico Alimin (nico@hackcapital.com)
+// date: Sunday, 5th August 2018 12:48:35 am
+// lastModifiedBy: Nico Alimin (nico@hackcapital.com)
+// lastModifiedTime: Sunday, 5th August 2018 12:52:42 am
+//
+// DESCRIPTION
+//
+// copyright (c) 2018 Nico Alimin
+////
+
+package main
+
+func main() {
+
+}
