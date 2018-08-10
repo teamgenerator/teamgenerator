7991a609d4132fce649f48ab33222ff0a730e754
Nico Alimin <nicoalimin@yahoo.com>
2018-08-09 21:11:52 -0700

    Created server backend with one Postgres Table (#21)
    * added initialization files
    
    * added Community Model
    
    * added the new files
    
    * added downloaded packages
    
    * added vendor
    
    * added the handlers
    
    * updated models to be specific to communities
    
    * typo in model name
    
    * renamed model to be in root folder
    
    * attempted to dockerize go
    
    * disabled ssl
    
    * got POST working
    
    * tested the requests
    
    * added dockerfile
    
    * added server to docker compose
    
    * added the new files
    
    * added package lock
    
    * added logger
    
    * added print statements
    
    * added vendor to .gitignore
    
    * removed vendor
    
    * added vendor to gitignore after deleting vendors
    
    * modified .gitignore to ignore vendor/
    
    * added stuff
    
    * ignored server vendor
    
    * added depedency manager to dockerfile
    
    * improve logs
    
    * changed email and added routing for /api/vi endpoint

--- server/main.go
+++ server/main.go
@@ -1,7 +1,7 @@
 ////
-// author: Nico Alimin (nico@hackcapital.com)
+// author: Nico Alimin (nicoalimin@gmail.com)
 // date: Sunday, 5th August 2018 12:48:35 am
-// lastModifiedBy: Nico Alimin (nico@hackcapital.com)
+// lastModifiedBy: Nico Alimin (nicoalimin@gmail.com)
 // lastModifiedTime: Sunday, 5th August 2018 12:52:42 am
 //
 // DESCRIPTION
@@ -11,6 +11,46 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
+var (
+	pgUser     = os.Getenv("PG_USER")
+	pgPassword = os.Getenv("PG_PASSWORD")
+	pgDatabase = os.Getenv("PG_DATABASE")
+	pgHost     = os.Getenv("PG_HOST")
+	pgPort     = os.Getenv("PG_PORT")
+	port       = 3030
+)
+
+func main() {
+	a := App{}
+
+	a.Initialize(
+		pgHost,
+		pgPort,
+		pgUser,
+		pgPassword,
+		pgDatabase,
+	)
+	fmt.Printf("Database initialized at %s for database %s\n", pgPort, pgDatabase)
+
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Initializing app\n")
+	a.Run(":" + strconv.Itoa(port))
 }
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS community
+(
+id SERIAL,
+name TEXT NOT NULL,
+location TEXT NOT NULL,
+CONSTRAINT community_pkey PRIMARY KEY (id)
+)`
