4f53a9f609cc904196696e193313f0041c1dbc81
nicoalimin <nicoalimin@yahoo.com>
2018-08-15 01:47:25 -0700

    added the postgres database file

---
+++ server/db/db.go
@@ -0,0 +1,39 @@
+////
+// author: Nico Alimin (nicoalimin@hotmail.com)
+// date: Tuesday, 14th August 2018 1:12:38 am
+// lastModifiedBy: Nico Alimin (nicoalimin@hotmail.com)
+// lastModifiedTime: Tuesday, 14th August 2018 1:12:38 am
+//
+// DESCRIPTION
+//
+// copyright (c) 2018 Nico Alimin
+////
+
+package db
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+// DB is the database we want to export
+var DB *gorm.DB
+
+// Open opens the database connection
+func Open(pgUser string, pgPassword string, pgDatabase string, pgHost string, pgPort string) {
+	connectionString :=
+		fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", pgHost, pgPort, pgUser, pgPassword, pgDatabase)
+
+	var err error
+	DB, err = gorm.Open("postgres", connectionString)
+	fmt.Printf("Opened connection to %s\n", connectionString)
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
+
+// Close closes the database connection
+func Close() error {
+	return DB.Close()
+}
