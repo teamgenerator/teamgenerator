a732ca2fe15003f66aa4960d1ad5404d95a62c17
nicoalimin <nicoalimin@yahoo.com>
2019-06-23 21:58:44 -0700

    Added other CRUD endpoints for session-players

--- server/models/sessionPlayer.model.go
+++ server/models/sessionPlayer.model.go
@@ -21,10 +21,46 @@ type SessionPlayer struct {
 	FormChange  int
 }
 
+// GetSessionPlayers function to return all ratings
+func GetSessionPlayers(w http.ResponseWriter, r *http.Request) {
+	var sessionPlayers []SessionPlayer
+	db.DB.Find(&sessionPlayers)
+	json.NewEncoder(w).Encode(&sessionPlayers)
+}
+
 // GetSessionPlayer function to get a single SessionPlayer
 func GetSessionPlayer(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	var sessionPlayer SessionPlayer
 	db.DB.First(&sessionPlayer, params["id"])
+	json.NewEncoder(w).Encode(&sessionPlayer)
+}
+
+// CreateSessionPlayer function to create a single SessionPlayer
+func CreateSessionPlayer(w http.ResponseWriter, r *http.Request) {
+	var sessionPlayer SessionPlayer
+	json.NewDecoder(r.Body).Decode(&sessionPlayer)
+	db.DB.Create(&sessionPlayer)
+	json.NewEncoder(w).Encode(&sessionPlayer)
+}
+
+// UpdateSessionPlayer function to update an existing SessionPlayer
+func UpdateSessionPlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var sessionPlayer SessionPlayer
+	db.DB.First(&sessionPlayer, params["id"])
+	json.NewDecoder(r.Body).Decode(&sessionPlayer)
+	db.DB.Save(&sessionPlayer)
+	json.NewEncoder(w).Encode(&sessionPlayer)
+}
+
+// DeleteSessionPlayer function to delete a single session-player joint table by ID
+func DeleteSessionPlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var sessionPlayer SessionPlayer
+	db.DB.First(&sessionPlayer, params["id"])
+	if sessionPlayer.ID != 0 {
+		db.DB.Delete(&sessionPlayer)
+	}
 	json.NewEncoder(w).Encode(&sessionPlayer)
 }
