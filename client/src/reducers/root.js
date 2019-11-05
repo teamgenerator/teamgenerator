54d8b607b50bc40c8298f1bd18572a51a9845fbe
nicoalimin <nicoalimin@yahoo.com>
2019-11-05 00:50:14 -0800

    Added sessions reducer

--- client/src/reducers/root.js
+++ client/src/reducers/root.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
-import players from './players';
+import { combineReducers } from "redux";
+import players from "./players";
+import sessions from "./sessions";
 
 export default combineReducers({
-    players,
+  players,
+  sessions
 });
