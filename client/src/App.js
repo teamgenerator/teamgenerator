b3e0b6313d28a65988c1b918ddf171448eb4570c
nicoalimin <nicoalimin@yahoo.com>
2019-11-05 00:56:05 -0800

    Added barebones sessions

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import configureStore from './store';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import configureStore from "./store";
 
-import HomeScreen from './containers/HomeScreen';
-import PlayersScreen from './containers/PlayersScreen';
-import PlayerDetails from './containers/PlayerDetails';
-import GenerateScreen from './containers/GenerateScreen';
-import SessionDetails from './containers/SessionDetails';
+import HomeScreen from "./containers/HomeScreen";
+import PlayersScreen from "./containers/PlayersScreen";
+import PlayerDetails from "./containers/PlayerDetails";
+import GenerateScreen from "./containers/GenerateScreen";
+import SessionDetails from "./containers/SessionDetails";
 
 const store = configureStore();
 class App extends Component {
-
   constructor(props) {
     super(props);
 
@@ -24,11 +23,11 @@ class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={HomeScreen} />
-
             // Router for players
             <Route exact path="/players" component={PlayersScreen} />
             <Route path="/players/:id" component={PlayerDetails} />
             <Route exact path="/generate" component={GenerateScreen} />
+            <Route exact path="/sessions" component={SessionDetails} />
             <Route path="/sessions/:id" component={SessionDetails} />
           </Switch>
         </BrowserRouter>
