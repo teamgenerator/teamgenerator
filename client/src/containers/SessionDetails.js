8cfebbd74f7d077757b8a494219316a9630c09cb
nicoalimin <nicoalimin@yahoo.com>
2019-11-05 00:59:06 -0800

    Reverted   session  details

--- client/src/containers/SessionDetails.js
+++ client/src/containers/SessionDetails.js
@@ -1,50 +1,42 @@
-import React, { Component } from "react";
-import {
-  Button,
-  Grid,
-  TextField,
-  IconButton,
-  Icon,
-  Typography
-} from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "../components/AppBar";
-import { connect } from "react-redux";
-import Save from "@material-ui/icons/Save";
-import { diff } from "deep-object-diff";
-import StarRatings from "../components/StarRatings";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
+import React, { Component } from 'react';
+import { Button, Grid, TextField, IconButton, Icon, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '../components/AppBar';
+import { connect } from 'react-redux';
+import Save from '@material-ui/icons/Save';
+import { diff } from 'deep-object-diff';
+import StarRatings from '../components/StarRatings';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
 const styles = {
   container: {
-    display: "flex",
-    flexDirection: "column"
+    display: 'flex',
+    flexDirection: 'column',
   },
   chevrons: {
-    left: "50%",
-    transform: "translateX(-50%)"
+    left: '50%',
+    transform: 'translateX(-50%)',
   },
-  ratingsGridItem: {}
+  ratingsGridItem: {
+  },
 };
 
-class SessionIndex extends Component {
+class SessionDetails extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       updatedAttributes: {
-        id: "001",
-        isActive: true
-      }
+        id: '001',
+        isActive: true,
+
+      },
     };
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const playerDiffBetweenPrevStateAndCurrState = diff(
-      prevState.updatedAttributes,
-      this.state.updatedAttributes
-    );
+    const playerDiffBetweenPrevStateAndCurrState = diff(prevState.updatedAttributes, this.state.updatedAttributes);
     if (Object.keys(playerDiffBetweenPrevStateAndCurrState).length > 0) {
       const playerDiff = diff(this.props.player, this.state.updatedAttributes);
       this.setState({ isDirty: Object.keys(playerDiff).length > 0 });
@@ -55,47 +47,38 @@ class SessionIndex extends Component {
     this.setState({
       updatedAttributes: {
         ...this.state.updatedAttributes,
-        ...updatedAttributes
+        ...updatedAttributes,
       }
     });
   }
 
   handleNameTextfieldChange(e) {
     this.setUpdatedAttributes({
-      name: e.currentTarget.value
-    });
+        name: e.currentTarget.value,
+      });
   }
 
   handleEditRatings(ratingsChange) {
     this.setUpdatedAttributes({
-      ratings: this.state.updatedAttributes.ratings + ratingsChange
+      ratings: this.state.updatedAttributes.ratings + ratingsChange,
     });
   }
 
   render() {
     const { classes } = this.props;
-    const title =
-      this.props.match.params.id !== "new"
-        ? this.props.player.name
-        : "Current Session";
+    const title = this.props.match.params.id !== 'new' ? this.props.player.name : 'Current Session';
     return (
       <div className={classes.container}>
-        <AppBar
-          title="Sessions"
-          rightButton={{
-            icon: <Save />,
-            onClick: () => alert("clicked"),
-            disabled: !this.state.isDirty
-          }}
-        />
+        <AppBar title={title} rightButton={{
+          icon: <Save />,
+          onClick: () => alert('clicked'),
+          disabled: !this.state.isDirty,
+        }}/>
         <Grid container spacing={24} alignItems="center">
           <Grid item xs={12}>
             <Typography variant="display1">New session has started</Typography>
-            <Typography variant="subheading">
-              Rate player's performance for this game. Your ratings will help us
-              get some insight of the players performance trend and adjust the
-              ratings overtime.
-            </Typography>
+            <Typography variant="subheading">Rate player's performance for this game. 
+            Your ratings will help us get some insight of the players performance trend and adjust the ratings overtime.</Typography>
           </Grid>
         </Grid>
       </div>
@@ -104,15 +87,13 @@ class SessionIndex extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  player:
-    Object.values(state.players).find(
-      player => player.id === ownProps.match.params.id
-    ) || {}
+  player: Object.values(state.players).find(player => player.id === ownProps.match.params.id) || {},
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(SessionIndex));
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SessionDetails));
