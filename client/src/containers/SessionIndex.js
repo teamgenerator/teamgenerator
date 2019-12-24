220ae823d337c3fb3b5ca96cae1da2dfaee8d4e8
nicoalimin <nicoalimin@yahoo.com>
2019-12-24 01:26:12 -0800

    Added barebones implementation of sessions

--- client/src/containers/SessionIndex.js
+++ client/src/containers/SessionIndex.js
@@ -5,7 +5,8 @@ import {
   List,
   ListItem,
   ListItemText,
-  ListItemSecondaryAction
+  ListItemSecondaryAction,
+  Divider
 } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "../components/AppBar";
@@ -19,23 +20,19 @@ const styles = {
     display: "flex",
     flexDirection: "column"
   },
+  fontRed: {
+    color: "red"
+  },
   chevrons: {
     left: "50%",
     transform: "translateX(-50%)"
-  },
-  ratingsGridItem: {}
+  }
 };
 
 class SessionIndex extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      updatedAttributes: {
-        id: "001",
-        isActive: true
-      }
-    };
+    this.state = {};
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -58,32 +55,17 @@ class SessionIndex extends Component {
     });
   }
 
-  handleNameTextfieldChange(e) {
-    this.setUpdatedAttributes({
-      name: e.currentTarget.value
-    });
-  }
-
-  handleEditRatings(ratingsChange) {
-    this.setUpdatedAttributes({
-      ratings: this.state.updatedAttributes.ratings + ratingsChange
-    });
-  }
-
-  renderSessionListItem(
-    sessionKey,
-    sessionName,
-    sessionDate,
-    sessionNumPlayers
-  ) {
+  renderSessionListItem(sessionKey, sessionDate, sessionNumPlayers) {
     return (
       <React.Fragment key={sessionKey}>
-        <ListItem>
-          <ListItemText primary={sessionName} />
+        <ListItem button>
+          <ListItemText primary={sessionDate} />
+          <ListItemText secondary={`Num TPlayers: ${sessionNumPlayers}`} />
           <ListItemSecondaryAction>
             <ChevronRight />
           </ListItemSecondaryAction>
         </ListItem>
+        <Divider />
       </React.Fragment>
     );
   }
@@ -100,26 +82,21 @@ class SessionIndex extends Component {
             disabled: !this.state.isDirty
           }}
         />
-        <List>
-          {this.props.sesionsArray.map(session => {
-            this.renderSessionListItem(
-              session.id,
-              this.props.activeCommunity,
-              session.Date,
-              1
-            );
-          })}
-        </List>
-        {/* <Grid container spacing={24} alignItems="center">
+        <Grid container spacing={24} alignItems="center">
           <Grid item xs={12}>
-            <Typography variant="display1">New session has started</Typography>
-            <Typography variant="subheading">
-              Rate player's performance for this game. Your ratings will help us
-              get some insight of the players performance trend and adjust the
-              ratings overtime.
+            <Typography variant="title">
+              <span className={classes.fontRed}>
+                {this.props.activeCommunity.Name}
+              </span>
+              {" sessions"}
             </Typography>
           </Grid>
-        </Grid> */}
+        </Grid>
+        <List>
+          {this.props.sessionsArray.map(session =>
+            this.renderSessionListItem(session.ID, session.CreatedAt, 1)
+          )}
+        </List>
       </div>
     );
   }
@@ -127,7 +104,7 @@ class SessionIndex extends Component {
 
 const mapStateToProps = state => ({
   sessions: state.sessions,
-  sesionsArray: Object.values(state.sessions),
+  sessionsArray: Object.values(state.sessions),
   activeCommunity: state.activeCommunity
 });
 
